841a506a76afe808282424b6c49b268f4d7d5602
vext01 <vext01@gmail.com>
2012-04-15 13:08:42 +0100

    note about Std.input_list

--- read_file_handle_exceptions.ml
+++ read_file_handle_exceptions.ml
@@ -8,3 +8,8 @@ let print_file name = let instream = open_in name in
         ) with End_of_file -> (close_in instream; ());;
 
 print_file "/etc/hosts";;
+
+(*
+ * Note that (Std.input_list chan) readsa file in to a list and may be better
+ * in some cases
+ *)
