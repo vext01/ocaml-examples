f533f7df8ff94e6926a8d246883a9f664c22d9e2
vext01 <vext01@gmail.com>
2012-04-17 18:08:41 +0100

    example of a map

---
+++ map.ml
@@ -0,0 +1,20 @@
+(*
+ * Make a module which defines a Map whose key is a string,
+ * the right hand size (values) remain polymorphic
+ *)
+
+module StringMap = Map.Make(String);;
+
+(* Instantiate our new map type *)
+let map = StringMap.empty;;
+
+(* Add some stuff *)
+let map = StringMap.add "Key1" 1 map;;
+let map = StringMap.add "Key2" 2 map;;
+let map = StringMap.add "Key3" 3 map;;
+
+(* This is how we would print our map *)
+let printMap m =
+  StringMap.iter (fun key value -> Printf.printf "%s -> %d\n" key value) m;;
+
+printMap map;;
